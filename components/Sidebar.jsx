5125e9a59be7bcb7e7812fb68dfd2dde48f899e3
Jeramai <j.faber@provrex.nl>
2022-10-05 21:39:13 +0200

    Added sidebar & save previous

---
+++ components/Sidebar.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef } from 'react';
+import style from '/styles/Home.module.scss';
+
+export default function Sidebar({ theWheel, segments, setSegments }) {
+  const segmentRef = useRef();
+
+  useEffect(() => {
+    if (theWheel?.segments) {
+      setSegments([...theWheel.segments]);
+    }
+  }, [theWheel?.segments]);
+
+  const randomFillStyle = () => {
+    const colorArray = ['#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226'];
+    return colorArray[(Math.random() * colorArray.length) | 0];
+  };
+
+  const addSegment = (e) => {
+    e.preventDefault();
+    if (!segmentRef.current.value) return;
+
+    const newSegment = {
+      text: segmentRef.current.value,
+      fillStyle: randomFillStyle()
+    };
+
+    theWheel.addSegment(newSegment);
+    theWheel.draw();
+
+    setSegments([...theWheel.segments]);
+    localStorage.setItem('segments', JSON.stringify(theWheel.segments));
+
+    segmentRef.current.value = '';
+  };
+  const deleteSegment = (i) => {
+    theWheel.deleteSegment(i);
+    theWheel.draw();
+
+    setSegments([...theWheel.segments]);
+    localStorage.setItem('segments', JSON.stringify(theWheel.segments));
+  };
+
+  return (
+    <div id='sidebarWrapper' className='container' style={{ width: '250px', height: '100%', backgroundColor: '#fef6c9' }}>
+      <ul className='py-3' style={{ padding: 0 }}>
+        {segments?.map((segment, i) => {
+          if (!segment?.text) return null;
+          return (
+            <li
+              key={i}
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between'
+              }}
+            >
+              {segment.text}
+              {theWheel?.segments?.filter((s) => s).length > 1 ? (
+                <span onClick={() => deleteSegment(i)} className={style.removeButton}>
+                  <i className='bi bi-trash-fill'></i>
+                </span>
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className='row'>
+        <div className='col'>
+          <form onSubmit={addSegment}>
+            <div className='input-group mb-3'>
+              <input
+                type='text'
+                className='form-control'
+                placeholder='More food..'
+                aria-label='Username'
+                aria-describedby='basic-addon1'
+                ref={segmentRef}
+              />
+              <button type='submit' className='input-group-text'>
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Wheel.jsx
+++ components/Wheel.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 
-export default function Wheel({ spinning, onStop }) {
-  const [theWheel, setTheWheel] = useState();
-
+export default function Wheel({ theWheel }) {
   useEffect(() => {
     const canvas = document.getElementById('canvas');
     const main = document.getElementById('wheelWrapper');
@@ -11,57 +9,7 @@ export default function Wheel({ spinning, onStop }) {
 
     canvas.width = main.offsetWidth;
     canvas.height = main.offsetHeight;
-
-    initWheel();
   }, []);
-
-  function initWheel() {
-    const segments = [
-      { fillStyle: '#eae56f', text: 'Segment 1' },
-      { fillStyle: '#89f26e', text: 'Segment 2' },
-      { fillStyle: '#7de6ef', text: 'Segment 3' },
-      { fillStyle: '#e7706f', text: 'Segment 4' }
-    ];
-    const params = {
-      numSegments: segments.length,
-      fillStyle: '#e7706f',
-      lineWidth: 1,
-      segments,
-      animation: {
-        type: 'spinToStop',
-        duration: 5, // In seconds
-        spins: 8,
-        callbackFinished: () => alertWin()
-      }
-    };
-    setTheWheel(new Winwheel(params));
-  }
-
-  function addSegment(text, fillStyle = 'random') {
-    if (fillStyle === 'random') {
-      const colors = ['#eae56f', '#89f26e', '#7de6ef', '#e7706f'];
-      fillStyle = colors[Math.floor(Math.random() * colors.length)];
-    }
-
-    // TODO: Maybe useful for adding the data to the chain
-    // http://dougtesting.net/winwheel/docs/tut5_add_remove_segments
-    theWheel.addSegment(
-      {
-        text,
-        fillStyle
-      },
-      1
-    );
-    theWheel.draw();
-  }
-  function deleteSegment(segment_id) {
-    theWheel.deleteSegment(segment_id);
-    theWheel.draw();
-  }
-  function alertWin() {
-    const winningSegment = theWheel.getIndicatedSegment();
-    alert('You won: ' + winningSegment.text);
-  }
 
   return (
     <div id='wheelWrapper' style={{ position: 'relative', width: '100%', height: '100%' }}>
@@ -70,16 +18,22 @@ export default function Wheel({ spinning, onStop }) {
         <Image src='/pin.png' width={45} height={45} />
       </div>
 
-      <button onClick={() => addSegment('Nice.')}>Add</button>
-      <button
-        style={{ marginTop: '10px' }}
-        onClick={() => {
-          theWheel.stopAnimation(false);
-          theWheel.startAnimation();
-        }}
-      >
-        Spin to win!
-      </button>
+      <div style={{ width: '100%', textAlign: 'center', marginTop: '10px' }}>
+        <button
+          className='btn btn-primary mt-2'
+          onClick={() => {
+            if (theWheel) {
+              theWheel.stopAnimation(false);
+
+              theWheel.rotationAngle = theWheel.rotationAngle % 360;
+
+              theWheel.startAnimation();
+            }
+          }}
+        >
+          Spin to win!
+        </button>
+      </div>
     </div>
   );
 }
